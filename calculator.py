e9a5f76bee09e97f1822202dfacc6a1a00bc7960
ramong <rlatndus21049@gmail.com>
2025-01-10 11:38:28 +0900

    d

--- calculator.py
+++ calculator.py
@@ -5,10 +5,8 @@ def add(a, b):
 def subtract(a, b):
   return a-b
 
-<<<<<<< HEAD
-def divide_Free(a, b):
-  return a/b
-=======
 def multiply(a, b):
   return a*b
->>>>>>> 362b237564c4386013a8030fee5cdcca098f4892
+
+def divide_Free(a, b):
+  return a/b
